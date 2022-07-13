9e87ce0c09ac8dbfb0e85518aa84164c630f1792
Luxzi <lesson085@gmail.com>
2022-07-12 23:28:56 -0400

    Changed space indenting to tabs

--- installer/install.rb
+++ installer/install.rb
@@ -3,43 +3,43 @@ require 'git'
 require 'fileutils'
 
 if OS.windows?
-    if File.directory? "C:/networx" 
-        puts "Networx is already installed. would you like to reinstall it? (y/n)"
-        confim = gets.chomp
-        if confim === "y"
-            FileUtils.remove_dir "C:/networx"
-        else
-            puts "Install Failed: User did not confirm reinstall, exiting..."
-            exit
-        end
-    end
+	if File.directory? "C:/networx" 
+		puts "Networx is already installed. would you like to reinstall it? (y/n)"
+		confim = gets.chomp
+		if confim === "y"
+			FileUtils.remove_dir "C:/networx"
+		else
+			puts "Install Failed: User did not confirm reinstall, exiting..."
+			exit
+		end
+	end
     
-    begin
-        Dir.chdir("C:/networx")
-    rescue
-    end
+	begin
+		Dir.chdir("C:/networx")	
+	rescue
+	end
 
-    puts "Cloning Repository..."
-    Git.clone "https://github.com/Furdev-Group/networx.git", "C:\\networx"
-    puts "Cloning Finished"
+	puts "Cloning Repository..."
+	Git.clone "https://github.com/Furdev-Group/networx.git", "C:\\networx"
+	puts "Cloning Finished"
     
-    File.delete "C:/networx/LICENSE"
-    File.delete "C:/networx/README.md"
-    File.delete "C:/networx/.gitignore"
-    FileUtils.remove_dir("C:/networx/.git")
+	File.delete "C:/networx/LICENSE"
+	File.delete "C:/networx/README.md"
+	File.delete "C:/networx/.gitignore"
+	FileUtils.remove_dir("C:/networx/.git")
     
-    puts "Install Completed: Please add C:/networx to your PATH variable, here is a guide: https://windowsloop.com/how-to-add-to-windows-path/"
-    puts "Install Info [1]: If you expirence issues running Networx: try using 'bundle' to install the dependencies while in the install folder"
-    puts "Install Info [2]: If this fails to fix the issue, please create an issue on github at: https://github.com/Furdev-Group/networx/issues"
-    exec "bundle >nul"
+	puts "Install Completed: Please add C:/networx to your PATH variable, here is a guide: https://windowsloop.com/how-to-add-to-windows-path/"
+	puts "Install Info [1]: If you expirence issues running Networx: try using 'bundle' to install the dependencies while in the install folder"
+	puts "Install Info [2]: If this fails to fix the issue, please create an issue on github at: https://github.com/Furdev-Group/networx/issues"
+	exec "bundle >nul"
 
-    
+    	
 end
 
 if OS.linux?
-    puts "Install Failed: Unsupported OS"
+	puts "Install Failed: Unsupported OS"
 end
 
 if OS.mac?
-    puts "Install Failed: Unsupported OS"
+	puts "Install Failed: Unsupported OS"
 end
