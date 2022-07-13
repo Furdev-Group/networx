e75901f4644e3b24f6da44f48ab57c8e2418a8e5
Luxzi <lesson085@gmail.com>
2022-07-12 23:23:12 -0400

    Refactor installer

--- installer/install.rb
+++ installer/install.rb
@@ -7,34 +7,33 @@ if OS.windows?
         puts "Networx is already installed. would you like to reinstall it? (y/n)"
         confim = gets.chomp
         if confim === "y"
-            FileUtils.remove_dir "C:\\networx"
+            FileUtils.remove_dir "C:/networx"
         else
             puts "Install Failed: User did not confirm reinstall, exiting..."
             exit
         end
     end
-
-    File.directory?("./networx") ? exists_error = true : nil
-
-    exists_error ? puts("Install Error: Repostory already exists in directory, would you like to delete it? (y/n)") : nil
-    exists_error ? confirm = gets.chomp : nil
-
-    if exists_error && confirm == "y"
-        FileUtils.rm_rf "./networx" 
+    
+    begin
+        Dir.chdir("C:/networx")
+    rescue
     end
 
     puts "Cloning Repository..."
-    Git.clone "https://github.com/Furdev-Group/networx.git", "networx"
+    Git.clone "https://github.com/Furdev-Group/networx.git", "C:\\networx"
     puts "Cloning Finished"
+    
+    File.delete "C:/networx/LICENSE"
+    File.delete "C:/networx/README.md"
+    File.delete "C:/networx/.gitignore"
+    FileUtils.remove_dir("C:/networx/.git")
+    
+    puts "Install Completed: Please add C:/networx to your PATH variable, here is a guide: https://windowsloop.com/how-to-add-to-windows-path/"
+    puts "Install Info [1]: If you expirence issues running Networx: try using 'bundle' to install the dependencies while in the install folder"
+    puts "Install Info [2]: If this fails to fix the issue, please create an issue on github at: https://github.com/Furdev-Group/networx/issues"
+    exec "bundle >nul"
 
-    puts "Moving files..."
-
-    temp = Dir[Dir.getwd + "/networx/*"]
-    temp.each do |filename|
-        File.rename filename, "C:/networx/#{filename.split("/").last}"
-    end
-
-    exec "set PATH=%PATH%;C:\\networx"
+    
 end
 
 if OS.linux?
