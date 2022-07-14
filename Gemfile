8b095b6b068c7bf1e0d560506b97d94622b19fd5
cylex <28271391+cylexVEVO@users.noreply.github.com>
2022-07-13 23:29:58 -0400

    fix adapters screen on linux

--- Gemfile
+++ Gemfile
@@ -7,4 +7,6 @@ gem "tty-prompt", "~> 0.23.1"
 gem "speedtest", "~> 0.2.3"
 gem "tty-spinner", "~> 0.9.3"
 gem "tty-table", "~> 0.12.0"
+gem "tty-platform", "~> 0.3.0"
+
 gem "win32-security", "~> 0.5.0" if Gem.win_platform?
--- features/adapters.rb
+++ features/adapters.rb
@@ -1,18 +1,21 @@
-require 'socket'
-require 'tty-table'
-require_relative '../util'
-require_relative '../config'
+require "socket"
+require "tty-table"
+require "tty-platform"
+require_relative "../util"
+require_relative "../config"
+
+$platform = TTY::Platform.new
 
 def adapters
     logo
 
-    $adapter_array = []
-
-    addrs = Socket.getifaddrs
+    addrs = Socket.getifaddrs if $platform.windows?
+    addrs = Socket.ip_address_list if not $platform.windows?
 
     selection = $prompt.select logo do |menu|
         addrs.map do |addr|
-            menu.choice "#{addr.name} | #{addr.addr.nil? ? "N/A" : addr.addr.ip_address}", addr
+            menu.choice("#{addr.name} | #{addr.addr.nil? ? "N/A" : addr.addr.ip_address}", addr) if $platform.windows?
+            menu.choice("#{addr.ip_address}", addr) if not $platform.windows?
         end
 
         menu.choice "main menu"
@@ -29,12 +32,20 @@ end
 def adapter(adapter)
     logo
 
-    name = adapter.name
-    type = "N/A" if adapter.addr.nil?
-    type = adapter.addr.ipv4? ? "IPv4" : "IPv6" if not adapter.addr.nil?
-    address = "N/A" if adapter.addr.nil?
-    address = adapter.addr.ip_address if not adapter.addr.nil?
-
+    if $platform.windows?
+        name = adapter.name
+        type = "N/A" if adapter.addr.nil?
+        type = adapter.addr.ipv4? ? "IPv4" : "IPv6" if not adapter.addr.nil?
+        address = "N/A" if adapter.addr.nil?
+        address = adapter.addr.ip_address if not adapter.addr.nil?
+    else
+        name = "N/A"
+        type = "N/A" if adapter.nil?
+        type = adapter.ipv4? ? "IPv4" : "IPv6" if not adapter.nil?
+        address = "N/A" if adapter.nil?
+        address = adapter.ip_address if not adapter.nil?
+    end
+    
     adapter_info_table = TTY::Table.new(["Adapter Name", "Adapter Type", "IP Address"], [[name, type, address]])
     renderer = TTY::Table::Renderer::Unicode.new(adapter_info_table, padding: [0, 1, 1, 1])
     puts renderer.render
